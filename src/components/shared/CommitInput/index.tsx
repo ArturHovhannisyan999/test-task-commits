95296d1a31004dda2863ee7657375cbb881501f6
ArturHovhannisyan999 <artur.hovhannisyan993@gmail.com>
2024-07-25 10:27:33 +0400

    add-debouncing

--- src/components/shared/CommitInput/index.tsx
+++ src/components/shared/CommitInput/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
-import { CommitInputProps } from "@/types";
+import React, { useEffect, useState } from 'react';
+import { CommitInputProps } from '@/types';
+import { useDebounce } from './lib/useDebounce';
 
 export const CommitInput: React.FC<CommitInputProps> = ({ searchTerm, setSearchTerm }) => {
+  const [localSearchTerm, setLocalSearchTerm] = useState(searchTerm);
+  const debouncedSearchTerm = useDebounce(localSearchTerm, 500);
+
+  useEffect(() => {
+    setSearchTerm(debouncedSearchTerm);
+  }, [debouncedSearchTerm, setSearchTerm]);
+
   return (
     <input
       type="text"
       placeholder="Search commits..."
-      value={searchTerm}
-      onChange={(e) => setSearchTerm(e.target.value)}
+      value={localSearchTerm}
+      onChange={(e) => setLocalSearchTerm(e.target.value)}
       className="p-2 w-80 border border-gray-300 rounded"
     />
   );
-}
+};
